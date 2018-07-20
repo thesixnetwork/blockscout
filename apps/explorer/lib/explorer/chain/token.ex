gkaemmer/poa-explorer

Add token transfer schema

apps/explorer/lib/explorer/chain/token.ex
@@ -1,0 +1,55 @@
+defmodule Explorer.Chain.Token do
+  @moduledoc """
+  Represents an ERC-20 token.
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+  alias Explorer.Chain.{Address, Hash, Token}
+
+  @typedoc """
+  * `:name` - Name of the token
+  * `:symbol` - Trading symbol of the token
+  * `:total_supply` - The total supply of the token
+  * `:decimals` - Number of decimal places the token can be subdivided to
+  * `:owner_address` - The `t:Address.t/0` of the owning wallet
+  * `:owner_address_hash` - Address hash foreign key
+  * `:contract_address` - The `t:Address.t/0` of the token's contract
+  * `:contract_address_hash` - Address hash foreign key
+  """
+  @type t :: %Token{
+          name: String.t(),
+          symbol: String.t(),
+          total_supply: Decimal.t(),
+          decimals: non_neg_integer(),
+          contract_address: Ecto.Association.NotLoaded.t() | Address.t(),
+          contract_address_hash: Hash.Truncated.t()
+        }
+
+  schema "tokens" do
+    field(:name, :string)
+    field(:symbol, :string)
+    field(:total_supply, :decimal)
+    field(:decimals, :integer)
+
+    belongs_to(
+      :contract_address,
+      Address,
+      foreign_key: :contract_address_hash,
+      references: :hash,
+      type: Hash.Truncated
+    )
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Token{} = token, params \\ %{}) do
+    token
+    |> cast(params, ~w(name symbol total_supply decimals contract_address_hash)a)
+    |> validate_required(~w(contract_address_hash))
+    |> foreign_key_constraint(:contract_address)
+    |> unique_constraint(:contract_address_hash)
+  end
+end

apps/explorer/lib/explorer/chain/token_transfer.ex
@@ -1,0 +1,95 @@
+defmodule Explorer.Chain.TokenTransfer do
+  @moduledoc """
+  Represents a token transfer between addresses for a given token.
+
+  ## Overview
+
+  Token transfers are special cases from a `t:Explorer.Chain.Log.t/0`. A token
+  transfer is always signified by the value from the `first_topic` in a log. That value
+  is always `0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef`.
+
+  ## Data Mapping From a Log
+
+  Here's how a log's data maps to a token transfer:
+
+  | Log                 | Token Transfer                 | Description                     |
+  |---------------------|--------------------------------|---------------------------------|
+  | `:second_topic`     | `:from_address_hash`           | Address sending tokens          |
+  | `:third_topic`      | `:to_address_hash`             | Address receiving tokens        |
+  | `:data`             | `:amount`                      | Amount of tokens transferred    |
+  | `:transaction_hash` | `:transaction_hash`            | Transaction of the transfer     |
+  | `:address_hash`     | `:token_contract_address_hash` | Address of token's contract     |
+  | `:index`            | `:log_index`                   | Index of log in transaction     |
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Explorer.Chain.{Address, Hash, Transaction, TokenTransfer}
+
+  @typedoc """
+  * `:amount` - The token transferred amount
+  * `:from_address` - The `t:Explorer.Chain.Address.t/0` that sent the tokens
+  * `:from_address_hash` - Address hash foreign key
+  * `:to_address` - The `t:Explorer.Chain.Address.t/0` that recieved the tokens
+  * `:to_address_hash` - Address hash foreign key
+  * `:token_contract_address` - The `t:Explorer.Chain.Address.t/0` of the token's contract.
+  * `:token_contract_address_hash` - Address hash foreign key
+  * `:transaction` - The `t:Explorer.Chain.Transaction.t/0` ledger
+  * `:transaction_hash` - Transaction foreign key
+  * `:log_index` - Index of the corresponding `t:Explorer.Chain.Log.t/0` in the transaction.
+  """
+  @type t :: %TokenTransfer{
+          amount: Decimal.t(),
+          from_address: Ecto.Association.NotLoaded.t() | Address.t(),
+          from_address_hash: Hash.Truncated.t(),
+          to_address: Ecto.Association.NotLoaded.t() | Address.t(),
+          to_address_hash: Hash.Truncated.t(),
+          token_contract_address: Ecto.Association.NotLoaded.t() | Address.t(),
+          token_contract_address_hash: Hash.Truncated.t(),
+          transaction: Ecto.Association.NotLoaded.t() | Transaction.t(),
+          transaction: Ecto.Association.NotLoaded.t() | Transaction.t(),
+          transaction_hash: Hash.Full.t(),
+          log_index: non_neg_integer()
+        }
+
+  @constant "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+
+  schema "token_transfers" do
+    field(:amount, :decimal)
+    field(:log_index, :integer)
+
+    belongs_to(:from_address, Address, foreign_key: :from_address_hash, references: :hash, type: Hash.Truncated)
+    belongs_to(:to_address, Address, foreign_key: :to_address_hash, references: :hash, type: Hash.Truncated)
+
+    belongs_to(
+      :token_contract_address,
+      Address,
+      foreign_key: :token_contract_address_hash,
+      references: :hash,
+      type: Hash.Truncated
+    )
+
+    belongs_to(:transaction, Transaction, foreign_key: :transaction_hash, references: :hash, type: Hash.Full)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%TokenTransfer{} = struct, params \\ %{}) do
+    struct
+    |> cast(params, ~w(amount log_index from_address_hash to_address_hash token_contract_address_hash transaction_hash))
+    |> validate_required(~w(log_index from_address_hash to_address_hash token_contract_address_hash))
+    |> foreign_key_constraint(:from_address)
+    |> foreign_key_constraint(:to_address)
+    |> foreign_key_constraint(:token_contract_address)
+    |> foreign_key_constraint(:transaction)
+  end
+
+  @doc """
+  Value that represents a token transfer in a `t:Explorer.Chain.Log.t/0`'s
+  `first_topic` field.
+  """
+  def constant, do: @constant
+end

thesixnetwork/blockscout

apps/explorer/lib/explorer/chain/token.ex
@@ -1,0 +1,53 @@
+defmodule Explorer.Chain.Token do
+  @moduledoc """
+  Represents an ERC-20 token.
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+  alias Explorer.Chain.{Address, Hash, Token}
+
+  @typedoc """
+  * `:name` - Name of the token
+  * `:symbol` - Trading symbol of the token
+  * `:total_supply` - The total supply of the token
+  * `:decimals` - Number of decimal places the token can be subdivided to
+  * `:contract_address` - The `t:Address.t/0` of the token's contract
+  * `:contract_address_hash` - Address hash foreign key
+  """
+  @type t :: %Token{
+          name: String.t(),
+          symbol: String.t(),
+          total_supply: Decimal.t(),
+          decimals: non_neg_integer(),
+          contract_address: %Ecto.Association.NotLoaded{} | Address.t(),
+          contract_address_hash: Hash.Address.t()
+        }
+
+  schema "tokens" do
+    field(:name, :string)
+    field(:symbol, :string)
+    field(:total_supply, :decimal)
+    field(:decimals, :integer)
+
+    belongs_to(
+      :contract_address,
+      Address,
+      foreign_key: :contract_address_hash,
+      references: :hash,
+      type: Hash.Address
+    )
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Token{} = token, params \\ %{}) do
+    token
+    |> cast(params, ~w(name symbol total_supply decimals contract_address_hash)a)
+    |> validate_required(~w(contract_address_hash))
+    |> foreign_key_constraint(:contract_address)
+    |> unique_constraint(:contract_address_hash)
+  end
+end

apps/explorer/lib/explorer/chain/token_transfer.ex
@@ -1,0 +1,95 @@
+defmodule Explorer.Chain.TokenTransfer do
+  @moduledoc """
+  Represents a token transfer between addresses for a given token.
+
+  ## Overview
+
+  Token transfers are special cases from a `t:Explorer.Chain.Log.t/0`. A token
+  transfer is always signified by the value from the `first_topic` in a log. That value
+  is always `0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef`.
+
+  ## Data Mapping From a Log
+
+  Here's how a log's data maps to a token transfer:
+
+  | Log                 | Token Transfer                 | Description                     |
+  |---------------------|--------------------------------|---------------------------------|
+  | `:second_topic`     | `:from_address_hash`           | Address sending tokens          |
+  | `:third_topic`      | `:to_address_hash`             | Address receiving tokens        |
+  | `:data`             | `:amount`                      | Amount of tokens transferred    |
+  | `:transaction_hash` | `:transaction_hash`            | Transaction of the transfer     |
+  | `:address_hash`     | `:token_contract_address_hash` | Address of token's contract     |
+  | `:index`            | `:log_index`                   | Index of log in transaction     |
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Explorer.Chain.{Address, Hash, Transaction, TokenTransfer}
+
+  @typedoc """
+  * `:amount` - The token transferred amount
+  * `:from_address` - The `t:Explorer.Chain.Address.t/0` that sent the tokens
+  * `:from_address_hash` - Address hash foreign key
+  * `:to_address` - The `t:Explorer.Chain.Address.t/0` that recieved the tokens
+  * `:to_address_hash` - Address hash foreign key
+  * `:token_contract_address` - The `t:Explorer.Chain.Address.t/0` of the token's contract.
+  * `:token_contract_address_hash` - Address hash foreign key
+  * `:transaction` - The `t:Explorer.Chain.Transaction.t/0` ledger
+  * `:transaction_hash` - Transaction foreign key
+  * `:log_index` - Index of the corresponding `t:Explorer.Chain.Log.t/0` in the transaction.
+  """
+  @type t :: %TokenTransfer{
+          amount: Decimal.t(),
+          from_address: %Ecto.Association.NotLoaded{} | Address.t(),
+          from_address_hash: Hash.Address.t(),
+          to_address: %Ecto.Association.NotLoaded{} | Address.t(),
+          to_address_hash: Hash.Address.t(),
+          token_contract_address: %Ecto.Association.NotLoaded{} | Address.t(),
+          token_contract_address_hash: Hash.Address.t(),
+          transaction: %Ecto.Association.NotLoaded{} | Transaction.t(),
+          transaction: %Ecto.Association.NotLoaded{} | Transaction.t(),
+          transaction_hash: Hash.Full.t(),
+          log_index: non_neg_integer()
+        }
+
+  @constant "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+
+  schema "token_transfers" do
+    field(:amount, :decimal)
+    field(:log_index, :integer)
+
+    belongs_to(:from_address, Address, foreign_key: :from_address_hash, references: :hash, type: Hash.Address)
+    belongs_to(:to_address, Address, foreign_key: :to_address_hash, references: :hash, type: Hash.Address)
+
+    belongs_to(
+      :token_contract_address,
+      Address,
+      foreign_key: :token_contract_address_hash,
+      references: :hash,
+      type: Hash.Address
+    )
+
+    belongs_to(:transaction, Transaction, foreign_key: :transaction_hash, references: :hash, type: Hash.Full)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%TokenTransfer{} = struct, params \\ %{}) do
+    struct
+    |> cast(params, ~w(amount log_index from_address_hash to_address_hash token_contract_address_hash transaction_hash))
+    |> validate_required(~w(log_index from_address_hash to_address_hash token_contract_address_hash))
+    |> foreign_key_constraint(:from_address)
+    |> foreign_key_constraint(:to_address)
+    |> foreign_key_constraint(:token_contract_address)
+    |> foreign_key_constraint(:transaction)
+  end
+
+  @doc """
+  Value that represents a token transfer in a `t:Explorer.Chain.Log.t/0`'s
+  `first_topic` field.
+  """
+  def constant, do: @constant
+end