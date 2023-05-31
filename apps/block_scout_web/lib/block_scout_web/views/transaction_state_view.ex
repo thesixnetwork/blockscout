bbnrepo/bbn-blockscout

Refactor state changes

apps/block_scout_web/lib/block_scout_web/views/transaction_state_view.ex
@@ -4,7 +4,7 @@ defmodule BlockScoutWeb.TransactionStateView do
   alias Explorer.Chain
   alias Explorer.Chain.Wei
 
-  import BlockScoutWeb.Models.TransactionStateHelper, only: [from_loss: 1, has_diff?: 1, to_profit: 1]
+  import Explorer.Chain.Transaction.StateChange, only: [from_loss: 1, has_diff?: 1, to_profit: 1]
 
   def not_negative?(%Wei{value: val}) do
     not Decimal.negative?(val)
@@ -26,15 +26,15 @@ defmodule BlockScoutWeb.TransactionStateView do
     has_diff?(from_loss(tx)) or has_diff?(to_profit(tx))
   end
 
-  def display_value(balance, :coin) do
+  def display_value(balance, :coin, _token_id) do
     format_wei_value(balance, :ether)
   end
 
-  def display_value(balance, token_transfer) do
-    render("_token_balance.html", transfer: token_transfer, balance: balance)
+  def display_value(balance, token_transfer, token_id) do
+    render("_token_balance.html", transfer: token_transfer, balance: balance, token_id: token_id)
   end
 
-  def display_nft(token_transfer) do
+  def display_721(token_transfer) do
     render(BlockScoutWeb.TransactionView, "_total_transfers.html", transfer: token_transfer)
   end
 end

thesixnetwork/blockscout

apps/block_scout_web/lib/block_scout_web/views/transaction_state_view.ex
@@ -4,7 +4,7 @@ defmodule BlockScoutWeb.TransactionStateView do
   alias Explorer.Chain
   alias Explorer.Chain.Wei
 
-  import BlockScoutWeb.Models.TransactionStateHelper, only: [from_loss: 1, has_diff?: 1, to_profit: 1]
+  import Explorer.Chain.Transaction.StateChange, only: [from_loss: 1, has_diff?: 1, to_profit: 1]
 
   def not_negative?(%Wei{value: val}) do
     not Decimal.negative?(val)
@@ -26,15 +26,15 @@ defmodule BlockScoutWeb.TransactionStateView do
     has_diff?(from_loss(tx)) or has_diff?(to_profit(tx))
   end
 
-  def display_value(balance, :coin) do
+  def display_value(balance, :coin, _token_id) do
     format_wei_value(balance, :ether)
   end
 
-  def display_value(balance, token_transfer) do
-    render("_token_balance.html", transfer: token_transfer, balance: balance)
+  def display_value(balance, token_transfer, token_id) do
+    render("_token_balance.html", transfer: token_transfer, balance: balance, token_id: token_id)
   end
 
-  def display_nft(token_transfer) do
+  def display_erc_721(token_transfer) do
     render(BlockScoutWeb.TransactionView, "_total_transfers.html", transfer: token_transfer)
   end
 end